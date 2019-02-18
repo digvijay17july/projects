7c4b873f1d6cae2e36ef8237426db81eb544e38b
Digvijay Singh <Digvijay.Singh@BDINDIA.LOCAL>
2019-02-18 14:23:04 +0530

    updated tax values

--- incomeTaxApp/src/main/java/com/pack/service/IncomeTaxService.java
+++ incomeTaxApp/src/main/java/com/pack/service/IncomeTaxService.java
@@ -2,10 +2,11 @@ package com.pack.service;
 
 public interface IncomeTaxService {
     Double educationCessPercent = 1.0;
-    Double generalSalarySlabs[] = {2500000.0, 5000000.0, 10000000.0};
-    Double seniorCitizenSalarySlabs[] = {3000000.0, 5000000.0, 10000000.0};
-    Double standardDeduction=0.0;
-    Double creditTax=2500.0;
+    Double generalSalarySlabs[] = {250000.0, 500000.0, 1000000.0};
+    Double seniorCitizenSalarySlabs[] = {300000.0, 500000.0, 1000000.0};
+    Double standardDeduction = 0.0;
+    Double creditTax = 2500.0;
+
     enum Category {SENIOR_CITIZEN, GENERAL;}
 
     Double computeIncomeTax(final double grossIncome, final int age);
@@ -23,26 +24,24 @@ public interface IncomeTaxService {
 
     default Double getTaxPercent(final double grossIncome, final int age) {
         Category category = getCategory(age);
-        double netIncome=grossIncome-educationCess(grossIncome)-standardDeduction;
+        double netIncome = grossIncome - educationCess(grossIncome) - standardDeduction;
         if (category == Category.GENERAL) {
 
-            return netIncome <= generalSalarySlabs[0] ? calculateTax(netIncome,0.0,generalSalarySlabs) : netIncome <= generalSalarySlabs[1] ? calculateTax(netIncome,5.0,generalSalarySlabs) : netIncome <= generalSalarySlabs[2] ? calculateTax(netIncome,20.0,generalSalarySlabs) :calculateTax(netIncome,20.0,generalSalarySlabs);
+            return netIncome <= generalSalarySlabs[0] ? calculateTax(netIncome, 0.0, generalSalarySlabs) : netIncome <= generalSalarySlabs[1] ? calculateTax(netIncome, 5.0, generalSalarySlabs) : netIncome <= generalSalarySlabs[2] ? calculateTax(netIncome, 20.0, generalSalarySlabs) : calculateTax(netIncome, 20.0, generalSalarySlabs);
 
 
         } else {
 
-            return grossIncome <= seniorCitizenSalarySlabs[0] ?  calculateTax(netIncome,0.0,seniorCitizenSalarySlabs) : grossIncome <= seniorCitizenSalarySlabs[1] ? calculateTax(netIncome,5.0,seniorCitizenSalarySlabs): grossIncome <= seniorCitizenSalarySlabs[2] ? calculateTax(netIncome,20.0,seniorCitizenSalarySlabs) :calculateTax(netIncome,20.0,seniorCitizenSalarySlabs) ;
+            return netIncome <= seniorCitizenSalarySlabs[0] ? calculateTax(netIncome, 0.0, seniorCitizenSalarySlabs) : netIncome <= seniorCitizenSalarySlabs[1] ? calculateTax(netIncome, 5.0, seniorCitizenSalarySlabs) : netIncome <= seniorCitizenSalarySlabs[2] ? calculateTax(netIncome, 20.0, seniorCitizenSalarySlabs) : calculateTax(netIncome, 20.0, seniorCitizenSalarySlabs);
         }
     }
-    default double calculateTax( final double netIncome,final double taxPercent, final Double taxSlabs[])
-    {
-        double totalTax=0.0;
-        if(taxPercent==5.0)
-        {
-            totalTax=((netIncome-taxSlabs[0])*taxPercent)/100;
-        }else if(taxPercent==20.0)
-        {
-            totalTax=Math.abs((((netIncome-taxSlabs[0])*5.0)+((netIncome-taxSlabs[0]-taxSlabs[1])*20.0))/100);
+
+    default double calculateTax(final double netIncome, final double taxPercent, final Double taxSlabs[]) {
+        double totalTax = 0.0;
+        if (taxPercent == 5.0) {
+            totalTax = (Math.abs(netIncome - taxSlabs[0]) * taxPercent) / 100;
+        } else if (taxPercent == 20.0) {
+            totalTax = Math.abs(((Math.abs(netIncome - taxSlabs[0]) * 5.0) + (Math.abs(netIncome - taxSlabs[0] - taxSlabs[1]) * 20.0)) / 100);
         }
         return totalTax;
     }
