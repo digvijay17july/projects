8eb93fd665199dec3b4ffd5d11287e8fd1c25a61
unknown <digvijay17july@gmail.com>
2023-04-24 00:02:35 +0530

    updated main app

---
+++ chatbot/src/main/java/com/demo/chatbot/ChatbotApplication.java
@@ -0,0 +1,14 @@
+package com.demo.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.*;
+import java.net.*;
+
+@SpringBootApplication
+public class ChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotApplication.class, args);
+	}
+}
